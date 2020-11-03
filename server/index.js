fea05c1870b67b722fa605cd2ae9903a074530ac
Jameson <githinjijmaeson@gmail.com>
2020-11-03 17:26:32 +0300

    clientSide

--- server/index.js
+++ server/index.js
@@ -1,27 +1,24 @@
-const express =require('express');
-const moongoose =require('mongoose');
+const express = require('express');
+const mongoose = require('mongoose');
 
-const houses=require ('./routes/houses')
+const houses = require('./routes/houses');
 
-const app =express();
+const app = express();
 
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send('Welcome to the house listing API')
+app.get('/', (req, res) => {
+    res.send('Welcome to the house listing API');
 });
 
-app.use('/api/houses',houses);
+app.use('/api/houses', houses);
 
 require('dotenv').config();
 
-const port=process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-moongoose.connect('mongodb+srv://j33my:PaRwgUhxbwzdV97b@passportauth.eki46.mongodb.net/nyumba_app?retryWrites=true&w=majority')
-.then(result=>{
-    app.listen(port,()=>console.log(`Server is running on port ${port}`))
-})
-.catch(err=>console.log(err))
-
-
-
+mongoose.connect('mongodb+srv://j33my:PaRwgUhxbwzdV97b@passportauth.eki46.mongodb.net/nyumba_app?retryWrites=true&w=majority')
+    .then(result => {
+        app.listen(port, () => console.log(`Server is running on port ${port}`))
+    })
+    .catch(err => console.log(err))
