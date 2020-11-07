4dc20eaf42f3b6cef9e5e35f66bf11a29b6972cb
Jameson Githinji <47845518+code-ninja-james@users.noreply.github.com>
2020-11-07 08:49:43 +0300

    Update House.js

--- server/models/House.js
+++ server/models/House.js
@@ -1,13 +1,13 @@
-const moongoose=require('mongoose');
+const mongoose = require('mongoose');
 
-const HouseSchema=new moongoose.Schema({
-    title:{type:String,required:true},
-    address:{type:String,required:true},
-    homeType:String,
-    description:String,
-    price:{type:Number,required:true},
-    image:String,
-    yearBuilt:Number
+const HouseSchema = new mongoose.Schema({
+    title: {type: String, required: true},
+    address: {type: String, required: true},
+    homeType: String,
+    description: String,
+    price: {type: Number, required: true},
+    image: String,
+    yearBuilt: Number
 })
 
- module.exports=moongoose.model('House',HouseSchema);
+module.exports = mongoose.model('House', HouseSchema);
